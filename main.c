b9ee2fa13874f8ff862a7b5802be3bcd622a49ee
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-13 21:34:58 -0600

    Update main.c
    put format to the table

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 
 int main() {
+    printf(".......TESTS........\n");
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    //funcion que llame a imprimir tabla de cables
+    printf("\n\n");
+    printf(".......Table of colors........\n");
     PrintColorPairTable();
     return 0;
 }
