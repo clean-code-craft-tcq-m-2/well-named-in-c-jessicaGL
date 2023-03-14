7305ac7d4dfa4402acad82bd26ab7c586a562687
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-13 22:50:30 -0600

    Update functions.h
    correcting errors

--- functions.h
+++ functions.h
@@ -12,6 +12,7 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+
 void PrintColorPairTable();
 
 #endif
